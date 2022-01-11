537ba08ce3de5b88f456647a178cfe0433039f89
marysol <marilirulita@gmail.com>
2022-01-11 15:59:58 -0800

    Comment display function

--- src/index.js
+++ src/index.js
@@ -24,46 +24,46 @@ const list = [
   },
 ];
 
-function display() {
-  list.sort((x, y) => x.index - y.index);
+// function display() {
+//   list.sort((x, y) => x.index - y.index);
 
-  const title = document.createElement('h4');
-  title.innerHTML = 'Today´s To Do';
-  const input = document.createElement('input');
-  input.type = 'text';
-  input.name = 'task';
-  input.id = 'newTask';
-  input.placeholder = 'Add to your list...';
-  const listElements = document.createElement('ul');
-  listElements.classList.add('listElements');
+//   const title = document.createElement('h4');
+//   title.innerHTML = 'Today´s To Do';
+//   const input = document.createElement('input');
+//   input.type = 'text';
+//   input.name = 'task';
+//   input.id = 'newTask';
+//   input.placeholder = 'Add to your list...';
+//   const listElements = document.createElement('ul');
+//   listElements.classList.add('listElements');
 
-  listElements.appendChild(title);
-  listElements.appendChild(input);
+//   listElements.appendChild(title);
+//   listElements.appendChild(input);
 
-  list.forEach((element) => {
-    const elementList = document.createElement('li');
-    const description = document.createElement('span');
-    description.innerHTML = element.description;
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    elementList.classList.add('elementList');
-    elementList.appendChild(checkBox);
-    elementList.appendChild(description);
-    listElements.appendChild(elementList);
-  });
+//   list.forEach((element) => {
+//     const elementList = document.createElement('li');
+//     const description = document.createElement('span');
+//     description.innerHTML = element.description;
+//     const checkBox = document.createElement('input');
+//     checkBox.type = 'checkbox';
+//     elementList.classList.add('elementList');
+//     elementList.appendChild(checkBox);
+//     elementList.appendChild(description);
+//     listElements.appendChild(elementList);
+//   });
 
-  const button = document.createElement('button');
+//   const button = document.createElement('button');
 
-  button.classList.add('delete-button');
-  button.innerHTML = 'Clear all completed';
-  button.type = 'button';
-  listElements.appendChild(button);
+//   button.classList.add('delete-button');
+//   button.innerHTML = 'Clear all completed';
+//   button.type = 'button';
+//   listElements.appendChild(button);
 
-  const listPlacehold = document.getElementById('list-placeholder');
-  listPlacehold.appendChild(listElements);
-}
+//   const listPlacehold = document.getElementById('list-placeholder');
+//   listPlacehold.appendChild(listElements);
+// }
 
-display();
+// display();
 
 function component() {
    const element = document.createElement('div');
